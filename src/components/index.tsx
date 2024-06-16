5926b2f2768c66d728b903f54510e4064a5516f0
Uchenna Ofor <ucheofor2015@gmail.com>
2024-06-16 19:26:59 +0100

    first commit

---
+++ src/components/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
+const HomeComponent = () => {
+  return (
+    <div>
+      <Navbar />
+
+      <Footer />
+    </div>
+  );
+};
+
+export default HomeComponent;
