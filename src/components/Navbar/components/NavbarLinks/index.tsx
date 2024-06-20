86e83db983d73f98bff1ed1c23a493b0522ba1e1
Ukwuoma Tochukwu <ukwuomatochi112@gmail.com>
2024-06-20 18:27:05 +0100

    started the navbar

---
+++ src/components/Navbar/components/NavbarLinks/index.tsx
@@ -0,0 +1,39 @@
+
+export const NavbarLinks = [
+  {
+    header: "Our Sections",
+    links: [
+      {
+        title: "About us",
+        path: "/",
+        description:
+          "What does GEMMA’s FOUNDATION do? We save lives, achieve social justice and have a role in preventing the neglect and abuse from children and adults"
+      },
+      {
+        title: "Our Services",
+        path: "/",
+        description: ""
+      },
+      {
+        title: "Our Mission",
+        path: "/",
+        description: ""
+      },
+      {
+        title: "Safeguarding children/young people",
+        path: "/",
+        description: "",
+      },
+      {
+        title: "Safeguarding Adults",
+        path: "/",
+        description: ""
+      },
+      {
+        title: "Contact Us",
+        path: "/",
+        description: ""
+      },
+    ],
+  }
+];
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import FirstSection from "./First-section/FirstSection";
 
 const HomeComponent = () => {
   return (
     <div>
       <Navbar />
-
+        <FirstSection />
       <Footer />
     </div>
   );
