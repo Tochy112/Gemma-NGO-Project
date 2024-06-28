8bb09d8eaaad3259d55fe8fa73f65620943323e0
Ukwuoma Tochukwu <ukwuomatochi112@gmail.com>
2024-06-28 08:28:42 +0100

    updated

---
+++ src/components/About/index.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import React from "react";
+
+const About = () => {
+  return (<div>
+    <h2 className="text-center my-10">
+    What does GEMMA FOUNDATION do? We save lives achieve social justice and have a role in preventing the neglect and abuse from children and adults,
+    </h2>
+  </div>);
+};
+
+export default About;
